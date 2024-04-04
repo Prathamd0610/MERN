5cadf794b050a077a4360a1f08095212e57ab07e
Pratham Dhingra <prathamdhingra14@gmail.com>
2024-04-04 20:49:04 +0530

    add file

---
+++ DAY3/Javascript/script.js
@@ -0,0 +1,150 @@
+// console.log('hello');
+//
+// function env()
+// {
+//     console.log(message);
+//     var message ='hello world';
+//     console.log(message);
+//     var message='else';
+//     console.log(message);
+    
+// }
+// env();
+// console.log(message);
+
+// function env()
+// {
+//     for(var i=0;i<5;i++)
+//     {
+//         console.log('--',i);
+//     }
+//     console.log(i);
+// }
+
+// env();
+
+// console.log(message);
+// let message= 'hello';
+// let message='else'
+
+//const
+// let message='abcd';
+// message='abbd';
+// console.log(message);
+// message=18;
+// console.log(message);
+// // alert('hello');
+
+// let arr=['a','b','c','d'];
+// arr[0]='abcd';
+// console.log(arr);
+// let str='12';
+// let age=20;
+// let ans=age+parseInt(str);
+// console.log(ans);
+// let a='12';
+// let b=12;
+// if(a==b)
+//     console.log('yes');
+// else 
+//     console.log('no');
+
+//     if(a===b)
+//     console.log('yes');
+// else 
+//     console.log('no');
+
+// let arr=['a','b','c','d'];
+// let copy = [...arr];
+// if(arr==copy)
+//     console.log('yes');
+// else 
+//     console.log('no');
+// const str ='lakhan';
+// const usrName= 'My name is : ';
+// console.log(` ${usrName} ${str}`);
+
+// function print(str='nothing is here'){
+//     console.log(str);
+// }
+// const s='hello';
+// print();
+
+// function sum(a,b) {
+//     if(a&&b)
+//     console.log(a+b);
+//     else if(a)
+//     console.log(a);
+//     else
+//     console.log(0);
+// }
+// sum(10,20);
+
+// FUNCTION DECLARATION
+// print();
+// a();
+// function print() {
+//     console.log('--');
+// }
+// // FUNCTION ASSIGNMENT(named)
+// let a=function print() {
+//     console.log('--');
+// }
+// // FUNCTION ASSIGNMENT(anonymous)
+// const b=function(){
+//     console.log('--');
+// }
+// // ARROW FUNCTION
+// let c= ()=>{
+//     console.log('--');
+// }
+// let a=2;
+// switch(a)
+// {
+//     case(1):{
+//         console.log('one');
+//         break;
+//     }
+//     case(2):{
+//         console.log('two')
+//         break;
+//     }
+//     case(3):{
+//         console.log('three')
+//         break;
+//     }
+//     default:{
+//         console.log('not 1,2,3')
+//         break;
+//     }
+// }
+
+
+// const obj= new Object();
+// obj[12]='twelve';
+// obj[1]='one';
+// obj[2]='tw0';
+// obj['name']='pratham'
+// const obj={
+//     1:'one',
+//     2:'two',
+//     'name':'pratham',
+// }
+// console.log(obj);
+// const obj={
+//     1:'one',
+//     'first':'pratham',
+//     'last':'dhingra',
+// }
+// const full= obj.first+' '+obj.last;
+// const full2= `${obj.first} ${obj.last}`;
+// console.log(full, full2);
+
+// const obj={
+//     'firstName':'ajay',
+//     'lastName':'kumar',
+//     'age':20,
+//     'address':'delhi',
+// }
+// const x=prompt('please tell the property to know :');
+// console.log(obj[x]);
